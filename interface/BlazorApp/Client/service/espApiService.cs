a7f5d1776744e565b28fa102d2e66707d3ffd684
_Leroy_Jenkins <89508010+CoreyJohnKight@users.noreply.github.com>
2023-11-07 15:50:05 +1300

    Setup ESP api service

---
+++ interface/BlazorApp/Client/service/espApiService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.services
+{
+    public class EspApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EspApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<int> GetHelloIntFromEspAsync()
+        {
+            var response = await _httpClient.GetStringAsync("http://192.168.1.35/hello");
+            Console.WriteLine("Response: " + response);
+
+            if (int.TryParse(response, out int result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+    }
+}
